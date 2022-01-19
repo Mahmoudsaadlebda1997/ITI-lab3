44f3b49a31634f863679266ebcea309a4c6d7df2
mahmoudlebda11997 <mahmodsaadlebda@gmail.com>
2022-01-19 22:30:26 +0200

    Lab 3 And 4 Fully Done

--- index.js
+++ index.js
@@ -38,6 +38,7 @@ git commit -m "message"
 git push
 */
 
+// Add users to /users
 app.post("/users", validateUser, async (req, res, next) => {
   try {
       const { username, age, password } = req.body;
@@ -49,14 +50,15 @@ app.post("/users", validateUser, async (req, res, next) => {
       await fs.promises.writeFile("./user.json", JSON.stringify(data), {
           encoding: "utf8",
       });
-      res.send({ id, message: "sucess" });
+      res.send({ id, message: " User Added sucessfully" });
   } catch (error) {
       next({ status: 500, internalMessage: error.message });
   }
 });
-
+//  edit Users By User ID
 app.patch("/users/:userId", validateUser, async (req, res, next) => {
   try {
+    
     const {username,password,age}=req.body;
     const users =await fs.promises.readFile("./user.json",{encoding:"utf8"})
     .then((data) => JSON.parse(data));
@@ -70,73 +72,112 @@ app.patch("/users/:userId", validateUser, async (req, res, next) => {
       };
     });
     await fs.promises.writeFile("./user.json",JSON.stringify(newUsers),{
-      encoding:"utf8"
-    });
-    res.status(200).send({message:"User Edited"});
+      encoding:"utf8"}
+      
+      );
+    res.status(200).send({message:"User Is  Edited Succesfully"});
   } catch (error){
     next({status:500,internalMessage:error.message});
   }
 });
 
+//  Get with age filter and return all users if no age is sent
 
 app.get('/users', async (req,res,next)=>{
   try {
-  const age = Number(req.query.age)
-  const users = await fs.promises
-  .readFile("./user.json", { encoding: "utf8" })
-  .then((data) => JSON.parse(data));
-  const filteredUsers = users.filter(user=>user.age===age)
-  res.send(filteredUsers)
+  if(typeof req.query.age == 'undefined')
+  {
+    const users = await fs.promises
+    .readFile("./user.json", { encoding: "utf8" })
+    .then((data) => JSON.parse(data));
+    res.send(users)
+  }else
+  {
+    const age = Number(req.query.age)
+    const users = await fs.promises
+    .readFile("./user.json", { encoding: "utf8" })
+    .then((data) => JSON.parse(data));
+    const filteredUsers = users.filter(user=>user.age===age)
+    res.send(filteredUsers)
+  }
+  
   } catch (error) {
   next({ status: 500, internalMessage: error.message });
   }
 
 })
 
-
+  //Log in user
+  app.post("/loginin",async (req, res, next) => {
+    const { username, password } = req.body;
+    if(!username) return next({status:422, message:"username is requird"})
+    if(!password) return next({status:422, message:"password is requird"})
+    try {
+      const users = await fs.promises
+      .readFile("./user.json", { encoding: "utf8" })
+      .then((data) => JSON.parse(data));
+      const isUser = users.some(user=>user.username===username && user.password ===password)
+      if(isUser)
+      {
+        res.status(200).send({message: "Succesfully Signned In"});
+      }else
+      {
+        next({status:403, message:"Please Register"})
+      }
+      } catch (error) {
+      next({ status: 500, internalMessage: error.message });
+      }
+  });
+// error Handler
 app.use((err,req,res,next)=>{
   if(err.status>=500){
     console.log(err.internalMessage)
     return res.status(500).send({error :"internal server error"})
-
   }
   res.status(err.status).send(err.message)
 
 })
+//delete users by ID
+app.delete("/users/:id", async (req, res, next) => {
+  try {
+      const id = req.params.id
+      console.log(id);
+      const users = await fs.promises.readFile('./user.json', { encoding: "utf8" })
+          .then((data) => JSON.parse(data))
 
-app.post("/login", (req, res) => {
-  const user = req.body
-  fs.readFile('./user.json', {}, (err, data) => {
-      if (err) {
-          console.log("File read failed:", err);
-          return;
-      }
-      const users  = JSON.parse(data)
-      var hasMatch
-      for (let element of users ) {
-          if (element.username == user.username &&
-              element.password == user.password) { hasMatch = true; break; }
-          hasMatch = false;
-      }
-      if (hasMatch == true) return res.status(200).send({ message: "login success" });
-      else {
-          return res.status(403).send({ error: "Login Failed" })
-      }
-  })
+      const newUsers = users.filter(user => {
+          return user.id != id
+      })
+      await fs.promises.writeFile("./user.json", JSON.stringify(newUsers), (err) => {
+          if (!err) return res.status(200).send({ message: "success" })
+          res.status(500).send("server error")
+      })
+      res.status(200).send({ message: "user deleted" })
+
+  }
+  catch (error) {
+      next({ status: 500, internalMessage: error.message });
+  }
 })
 
+// get users by id
 
-app.get("/users", (req, res) =>{
-  fs.readFile('./user.json', {}, (err, data)=>{
-      if (err) {
-          console.log("Cant Read File:", err);
-          return;
-      }
-      const users = JSON.parse(data)
-      res.send(users)
-  })
-});
+app.get("/users/:id", async (req, res, next) => {
+  try {
+      const id = req.params.id
+      console.log(id);
+      const users = await fs.promises.readFile('./user.json', { encoding: "utf8" })
+          .then((data) => JSON.parse(data))
 
+      const newUser = users.filter(user => {
+          return user.id == id
+      })
+      res.status(200).send(newUser)
+  }
+  catch (error) {
+      next({ status: 500, internalMessage: error.message });
+  }
+})
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
